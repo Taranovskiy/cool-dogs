c62a1a03c0b01017b0cd68361f8e7aa4d46d3fe7
Vladimir Taranovskiy <15webtaranovskiy@gmail.com>
2020-05-16 18:24:37 +0300

    add favorites link

---
+++ src/components/FavoritesLink.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-link
+    :to="{ name: 'favorites' }"
+    class="favorites-link"
+    active-class="favorites-link--active"
+  >
+    Избранные пёсели
+    <heart-icon class="favorites-link__icon" />
+  </router-link>
+</template>
+
+<script>
+import HeartIcon from "@/assets/icons/heart.svg";
+
+export default {
+  name: "FavoritesLink",
+  components: {
+    HeartIcon
+  }
+};
+</script>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -4,18 +4,22 @@
       <div class="main-header__logo">
         <the-logo />
       </div>
-      <div class="main-header__favorites">Favorites</div>
+      <div class="main-header__favorites">
+        <favorites-link />
+      </div>
     </div>
   </header>
 </template>
 
 <script>
 import TheLogo from "@/components/TheLogo";
+import FavoritesLink from "@/components/FavoritesLink";
 
 export default {
   name: "TheHeader",
   components: {
-    TheLogo
+    TheLogo,
+    FavoritesLink
   }
 };
 </script>
